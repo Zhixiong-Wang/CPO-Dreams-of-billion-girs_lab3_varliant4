768bc6ea4dbb03d884021a00b161205354caa34f
Wang Zhixiong <39585522+Zhixiong-Wang@users.noreply.github.com>
2022-06-16 19:35:10 +0800

    Update Moore_FSM.py

--- Moore_FSM.py
+++ Moore_FSM.py
@@ -88,33 +88,6 @@ class StateMachine(object):
             print("limit reached")
         return state
 
-    def visualize(self):
-
-        res = []
-        res.append("digraph G {")
-        res.append(" rankdir=LR;")
-        for v in self.inputs:
-            res.append(" {}[shape=rarrow];".format(v))
-        for v in self.outputs:
-            res.append(" {}[shape=rarrow];".format(v))
-        for i, n in enumerate(self.nodes):
-            res.append(' n_{}[label="{}"];'.format(i, n.name))
-        for i, n in enumerate(self.nodes):
-            for v in n.inputs:
-                if v in self.inputs:
-                    res.append(' {} -> n_{};'.format(v, i))
-            for j, n2 in enumerate(self.nodes):
-                if i == j:
-                    continue
-                for v in n.inputs:
-                    if v in n2.outputs:
-                        res.append(
-                            ' n_{} -> n_{}[label="{}"];'.format(j, i, v))
-            for v in n.outputs:
-                if v in self.outputs:
-                    res.append(' n_{} -> {};'.format(i, v))
-        res.append("}")
-        return "\n".join(res)
 
 
 class Node(object):
